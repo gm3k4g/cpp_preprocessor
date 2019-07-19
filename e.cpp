0db159bce0ce413a0af797752a400f49cf1859e6
g_oddball <driftolost@gmail.com>
2019-07-19 15:17:35 +0300

    code added

---
+++ e.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+namespace OUa = std;
+using FMawkifv = int;
+using GMDFNnvvaw = int;
+std::string OHdoHodgsFGAFSD = "Hello world";
+
+#define AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaa 13
+#define HLHYSHITSGDSOSAVBOSDFdoosdooboobdooo 55
+#define VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVaaaaaaaaaaaaaaaaaaaaaaaaaaaaaEEEeeeEEEEEE 99
+
+#define MMMMMMMMMmfBB main
+#define O OUa::cout
+#define MMmvvmvvMVv OUa::endl
+#define VVVFmsfwansfa OUa::cout
+#define MVnvnvawnwerfaw OUa::endl
+
+#define AOOAavsaSAVAOAAOAovoaOVAvsaVOASOVAOOA BDvvq3iqofjfnffn
+#define EEEEEEEEEEEEE000__EEEEaEEEEEEEEEEEEEEEEEEa VNNVnvnvnanwneanfa2i
+#define OEWIRwoirgitiogppsplzx_Awfawfakwfk2o3RFA koerko33rFQV3vr4Va
+
+#define OVAwjnvVIFIVfi BDvvq3iqofjfnffn
+#define FAKFooaweowaoefoewo434rEEEEEevvVVv VNNVnvnvnanwneanfa2i
+#define koerko33rFQV3vr4Va Amo0rwafegi9mr
+
+#define AWEawert3rt354r2 OUa::cout
+#define VMmvaewivi499r43g9 OUa::endl
+
+#define eEEEEvEFPVpeaVpe OUa::cout
+#define OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO OUa::endl
+
+
+FMawkifv OMMmmmmmmmmmmmmmmm() {return 0;}
+
+
+FMawkifv MMMMMMMMMmfBB(){
+	O << OHdoHodgsFGAFSD << MMmvvmvvMVv;
+	GMDFNnvvaw BDvvq3iqofjfnffn, VNNVnvnvnanwneanfa2i, Amo0rwafegi9mr;
+	AOOAavsaSAVAOAAOAovoaOVAvsaVOASOVAOOA = AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaa;
+	EEEEEEEEEEEEE000__EEEEaEEEEEEEEEEEEEEEEEEa = HLHYSHITSGDSOSAVBOSDFdoosdooboobdooo;
+	OEWIRwoirgitiogppsplzx_Awfawfakwfk2o3RFA = VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVaaaaaaaaaaaaaaaaaaaaaaaaaaaaaEEEeeeEEEEEE;
+	VVVFmsfwansfa << OVAwjnvVIFIVfi << MVnvnvawnwerfaw;
+	AWEawert3rt354r2 << FAKFooaweowaoefoewo434rEEEEEevvVVv << VMmvaewivi499r43g9;
+	eEEEEvEFPVpeaVpe << koerko33rFQV3vr4Va << OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO;
+	OMMmmmmmmmmmmmmmmm();
+}
